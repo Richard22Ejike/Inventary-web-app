85f7b8d930d71c1931a8e138e0dff146f2d14965
Richard dev <richard.ekene@aun.edu.ng>
2023-06-09 14:03:31 +0100

    header

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Add the CORS middleware and configure the allowed origins
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
@@ -38,14 +40,13 @@ app.get("/", (req, res) => {
 
 // Error Middleware
 app.use(errorHandler);
+
 // Connect to DB and start server
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("Connection successful");
-
-   
   })
   .catch((error) => console.log(`${error} did not connect`));
-  app.listen(PORT,"0.0.0.0", () => console.log(`Server Port: ${PORT} connected`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server Port: ${PORT} connected`));
